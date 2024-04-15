e0ca896104b7aa4150cf93a7fc9038f3404289e3
Pradeep yadav <126454840+PradeepCodes38@users.noreply.github.com>
2024-04-15 22:56:31 +0530

    Generate-random-string.tf

--- DAY-1/Generate random string.tf
+++ DAY-1/Generate random string.tf
@@ -1,1 +1,18 @@
 
+resource "random_string" "my_random_string" {
+
+# Specify the length of the random string (e.g., 10 characters)
+  length  = 10  
+# Include special characters in the random string (e.g., !@#$%^&*)
+  special = true  
+# Include uppercase letters in the random string
+  upper   = true  
+ # Include lowercase letters in the random string
+  lower   = true 
+# Include numbers in the random string
+  number  = true  
+}
+
+output "random_string_value" {
+  value = random_string.my_random_string.result
+}
